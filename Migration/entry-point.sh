8668221db8184733079cd868314d547e2f73f275
weijian <cnweijiany@gmail.com>
2020-10-24 20:22:48 +0800

    use docker-compose to start sql and run migration.

--- Migration/entry-point.sh
+++ Migration/entry-point.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-dotnet-fm migrate -c 'Server=webapp-sql;Database=WebApp;User Id=sa;Password=WJY@123456;' \
+dotnet-fm migrate -c "Server=${DATABASE_SERVER};Database=${DATABASE_NAME};User Id=sa;Password=${SA_PASSWORD};" \
     -p SqlServer2016 \
     -a ./out/Migration.dll
