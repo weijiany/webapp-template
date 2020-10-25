eb4b392d80f10beef2ee9d804c0b3762a32ec5d7
weijian <cnweijiany@gmail.com>
2020-10-25 12:05:43 +0800

    add max retry when start db.

--- data/entry-point.sh
+++ data/entry-point.sh
@@ -10,6 +10,26 @@ IF (NOT EXISTS (SELECT * FROM sys.databases where [name] = '${DATABASE_NAME}'))
 GO
 EOF
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SA_PASSWORD}" -i setup.sql -t 30 -l 30
+# the max times is 10
+times=0
+while [ $times -le 10 ]
+do
+  /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "${SA_PASSWORD}" -i setup.sql -t 30 -l 30
+
+  result=$?
+  if [ $result -eq '0' ]; then
+    echo ""
+    break
+  fi
+
+  sleep 5
+  times=$((times + 1))
+  echo "-------------retry times: ${times}"
+done
+
+if [ $times -eq 10 ]; then
+  echo "-------------retry to ${times}, exist 1"
+  exit 1
+fi
 
 fg 1
